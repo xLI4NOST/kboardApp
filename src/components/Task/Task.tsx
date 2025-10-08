ec4c37a477cc9e7b9d1288290d4d7ee9317e15ed
Dima Volkov <xli4nost@mail.ru>
2025-10-08 18:47:23 +0300

    hotRemark

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -25,7 +25,7 @@ export const Task = ({id, description, priority, title}: TaskI) => {
         transition,
     };
 
-    return <div ref={setNodeRef} style={style}  {...listeners}
+    return <div ref={setNodeRef} style={style} {...attributes} {...listeners}
                 className='rounded-xl bg-white shadow-[0_10px_0_0_rgba(0,0,0,0.15)] flex flex-col items-start gap-[10px] p-[20px] w-[350px] cursor-grab'>
         <h2 className='text-black'>{title}</h2>
         <p className='text-[#363636]'>{description}</p>
